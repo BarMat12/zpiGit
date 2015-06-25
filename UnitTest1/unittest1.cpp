160349cf9536306bb575122cf0c38c01a6feac8f
BarMat12 <bmatiaszek@gmail.com>
2015-06-25 18:41:29 +0200

    NAprawa problemu z testami

---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\zpi_git\zpi_git.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(run)
+		{
+		}
+	};
+}
